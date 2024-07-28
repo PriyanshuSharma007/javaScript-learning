ca7d3a789aec3388a6c1ad585ffc8c3039b6210f
PriyanshuSharma007 <143121050+PriyanshuSharma007@users.noreply.github.com>
2024-07-28 20:39:06 +0000

    comparison of value

---
+++ .devcontainer/01_basic/conversion.js
@@ -0,0 +1,11 @@
+//  here we learn the conversion in javaScript 
+
+console.log(null>0);  //it convert null to NaN
+console.log(null==0);  //Same as above
+console.log(null>=0);   // to the number that is zero (0)
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// NOTE** ---> TRY TO AVOID THESE TYPE OF CONVERSION IN JavaScript AS THEY ARE NOT PROPER WAY TO CONVERT 
