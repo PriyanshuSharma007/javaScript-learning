79735da11639cad9433ad5ebc2ff954a61cd9fc9
PriyanshuSharma007 <143121050+PriyanshuSharma007@users.noreply.github.com>
2024-07-28 21:53:28 +0000

    string

---
+++ .devcontainer/01_basic/string.js
@@ -0,0 +1,7 @@
+let Name= "Priyanshu" 
+let age= 23 
+let mobNo= "1234567890"  
+
+/* this is called conactenation in java script and it called string interopolation
+we can add any thing in the same line using dollar and curly braces  */ 
+console.log(`hey my name is  ${Name} and i m ${age} year old and my phone number is ${mobNo} `);
